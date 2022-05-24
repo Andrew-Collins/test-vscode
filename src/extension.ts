fea6a6d2c424592a15ef49721124618035afe6ec
andrewc <andrew@micromleon.com.au>
2022-05-24 16:33:16 +1000

    Class instance sending to terminal and output channel. Need to wait on promise from input.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as cp from "child_process";
+import { strictEqual } from 'assert';
 
 const execShell = (cmd: string) =>
     new Promise<string>((resolve, reject) => {
@@ -13,14 +14,48 @@ const execShell = (cmd: string) =>
         });
     });
 
+class RoverCode {
+	outC: vscode.OutputChannel;
+	term: vscode.Terminal;
+
+	constructor () {
+		this.outC = vscode.window.createOutputChannel("roverOut");
+		this.term = vscode.window.createTerminal("Rover");
+	}
+
+	connect(): void {
+		const serial = vscode.window.showInputBox({
+			placeHolder: 'Number on the main screen of the rover; e.g 42',
+			title: 'Rover Serial Number',
+			validateInput: text => {
+				if (text.trim() === '') return null;
+				else {
+					return Number.isNaN(Number(text)) ? 'Not a valid number' : null;
+				}
+			}
+		});
+		this.outC.append("Serial: " + serial);
+		this.term.sendText("try:");
+		this.term.sendText("from micromelon import *");
+		this.term.sendText("rc = RoverController()");
+		this.term.sendText("rc.connectBLE("+serial+")");
+		this.term.sendText("except error as e:");
+		this.term.sendText("print(e)");
+	}
+}
+
+let rover: RoverCode;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+	rover = new RoverCode();
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "micromelon" is now active!');
 
+
+	rover.term.sendText("python3");
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -32,18 +67,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(disposable);
 
-	vscode.commands.registerCommand('micromelon.program', () => {
-		const x = 1;
-		let outC = vscode.window.createOutputChannel("rover");
-		let temp = cp.exec("echo " + x, (err, out) => {
-            if (err) {
-                return 0;
-            }
-           	outC.appendLine(out);
-			outC.show(true);
-			return 1;
-        });
-	});
+	vscode.commands.registerCommand('micromelon.program', rover.connect.bind(rover));
 
 }
 
